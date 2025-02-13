bee1fbf0a35e59939b33e94a7eb41677d0d272a5
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-02-13 22:31:35 +0100

    Ajout de l'interactivité : liste de produits avec bouton supprimer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import React from "react";
-import "./App.css"; 
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [products, setProducts] = useState([
+    //Declaration d'un tableau d'objets
+    { id: 1, name: "Produit A", price: 100 },
+    { id: 2, name: "Produit B", price: 150 },
+    { id: 3, name: "Produit C", price: 200 },
+  ]);
+
+
+  const deleteProduct = (id) => {
+    const newProducts = products.filter((product) => product.id !== id);
+    setProducts(newProducts);
+  }
+
+
+
   return (
     <div id="container">
-      <h1>Tableau de Bord</h1>
-      <p>Ceci est une application de debutant</p>
-      
+      <h1>Tableau de Bord de produits</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price} $
+            <button onClick={() => deleteProduct(product.id)}>Supprimer
+
+            </button>
+          </li>
+        ))}
+      </ul>
+
     </div>
   )
 }
