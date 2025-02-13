b187a2091a8d564c72b55d42d9de37cdf01d16fb
NanteRanaivosoa <rnantenainamaminirina@gmail.com>
2025-02-13 22:46:34 +0100

    Ajout de la mise en page avec Flexbox et Grid

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     { id: 1, name: "Produit A", price: 100 },
     { id: 2, name: "Produit B", price: 150 },
     { id: 3, name: "Produit C", price: 200 },
+    { id: 4, name: "Produit D", price: 350 },
   ]);
 
 
